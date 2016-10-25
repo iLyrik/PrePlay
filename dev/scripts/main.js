6b6ae164e5a20c36aa68452addba92c6d3ae5037
Erika Donovan <erkdonovan@gmail.com>
2016-10-25 16:39:39 -0400

    adding in the ajax call and api key to info

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,5 +1,17 @@
 var appSong = {};
 
-appSong.getSongs = function () {
-  $.ajax 
+appSong.getMetro = function (metroID) {
+  $.ajax ({
+    console.log('appSong.getMetro', arguments);
+    url: http://api.songkick.com/api/3.0/metro_areas/{metro_area_id}/calendar.json,
+    method: 'GET',
+    format: 'json',
+    data: {
+      apikey: 'hHSjLHKTmsfByvxU',
+      q: metroID
+    }
+  }).then(function(metroLocation) {
+    metroLocation = metroLocation.location.metro-area.id;
+    console.log(metroLocation);
+  });
 }
