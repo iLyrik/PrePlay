df807889c9567066991827cb325d3479bbb432c5
Erika Donovan <erkdonovan@gmail.com>
2016-10-25 17:50:07 -0400

    JS changes

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,17 +1,65 @@
 var appSong = {};
-// test
-appSong.getMetro = function (metroID) {
+
+//returns list of cities that match query name
+appSong.getMatchingCities = function (city) {
   $.ajax ({
-    // console.log('appSong.getMetro', arguments);
-    url: 'http://api.songkick.com/api/3.0/metro_areas/{metro_area_id}/calendar.json',
+    url: 'http://api.songkick.com/api/3.0/search/locations.json',
     method: 'GET',
-    format: 'json',
+    dataType: 'json',
     data: {
-      apikey: 'hHSjLHKTmsfByvxU',
-      q: metroID
+      query: city,
+      apikey: 'hHSjLHKTmsfByvxU'
     }
   }).then(function(metroLocation) {
-    metroLocation = metroLocation.location.metro-area.id;
-    console.log(metroLocation);
+    metroLocation = metroLocation;
+    appSong.displayLocations(metroLocation.resultsPage.results.location)
+    //console.log(metroLocation);
   });
 }
+
+appSong.getMetroId = function (metroID) {
+  $.ajax ({
+    url: 'http://api.songkick.com/api/3.0/metro_areas/{metro_area_id}/calendar.json',
+    method: 'get',
+    dataType: 'json',
+    data: {
+      q: metroID,
+      apikey: 'hHSjLHKTmsfByvxU'
+    }
+  });
+}
+
+
+//when user enters city in search field, take value and search in appSong.getMatchingCities
+appSong.usersLocation = function() {
+
+  $('form').on('submit', function() {
+    var usersLocation = $("input[type=search]").val()
+    appSong.getMatchingCities(usersLocation);
+  });
+
+} //appSong.usersLocation
+
+
+
+//display matching cities and have user select the correct one 
+appSong.getLocations = function(locationResults){
+
+  locationResults = locationResults.filter(function(locationLoop) {
+      console.log(locationLoop);
+  });
+
+} //appSong.getLocations
+
+
+
+//take the matching metro id and enter into getMetroID
+
+//then display the concerts in that metro id
+appSong.init = function() {
+  appSong.usersLocation();
+}
+
+$(function() {
+  appSong.init();
+});
