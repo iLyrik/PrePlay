9acf22f4a47661ceed0149b1a0dc2349a6c1784c
Erika Donovan <erkdonovan@gmail.com>
2016-10-26 18:02:03 -0400

    location searching

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,26 +1,37 @@
 var appSong = {};
 
+appSong.city = ''
+appSong.state = ''
+appSong.country = ''
+
+//this is the start of everything
 //STEP 1
 //when user enters city in search field, take value and search in appSong.getMatchingCities
-appSong.usersLocation = function() {
+appSong.usersLocation = function(city) {
     $('.cities').hide();
 
   $('.locationInput').on('submit', function(e) {
     $('.cities').show();
     e.preventDefault();
     var location = $('#autocomplete').val();
+    //take the location that the user entered and split into an array
     location = location.split(',')
+
     console.log(location)
-    var city = location[0]
-    var state = location[1]
-    var country = location[2]
-    // appSong.getMatchingCities(city);
+    
+    //assigning the answers to different objects
+    appSong.city = location[0]
+    appSong.state = location[1]
+    appSong.country = location[2]
+    
+    appSong.getMatchingCities(appSong.city);
+
   });
 
 } //appSong.usersLocation
 
 
-//1.a autocomplete the input for city selection
+//STEP 1A autocomplete the input for city selection
 appSong.getLocations = function(){
      var autocomplete = new google.maps.places.Autocomplete(
             (document.getElementById('autocomplete')),
@@ -29,6 +40,7 @@ appSong.getLocations = function(){
 
 
 } //appSong.getLocations
+
 
 //STEP 2
 //returns list of cities that match query name
@@ -45,92 +57,22 @@ appSong.getMatchingCities = function (city) {
   }).then(function(metroLocation) {
     metroLocation = metroLocation.resultsPage.results.location
     //gives appSong.displayLocation the results
-    appSong.displayLocation(metroLocation);
+    appSong.matchLocations(metroLocation);
     console.log('appSong.getMatchingCities', metroLocation);
   });
 }
 
 
-//STEP 4
-// returns the metro ID of the user selected city
-appSong.getMetroId = function (metroID) {
-  $.ajax ({
-    url: `http://api.songkick.com/api/3.0/metro_areas/${metroID}/calendar.json`,
-    method: 'get',
-    dataType: 'json',
-    data: {
-      apikey: 'hHSjLHKTmsfByvxU'
-    }
-  }).then(function(bandReturn) {
-    bandReturn = bandReturn.resultsPage.results.event
-    appSong.displayConcerts(bandReturn)
-    console.log('return bands playing', bandReturn)
-  });
+//STEP 3
+appSong.matchLocations = function(matchLocations) {
+
+  if (appSong.city === matchLocations.displayName && appSong.country === matchLocations.country.displayName) {
+    return matchLocations.id
+  } else {
+    alert('No matching location');
+  }
+  console.log('matchLocations', matchLocations.id)
 }
-
-//this is the start of everything
-//STEP 1
-//when user enters city in search field, take value and search in appSong.getMatchingCities
-appSong.usersLocation = function() {
-    $('.cities').hide();
-
-  $('.locationInput').on('submit', function(e) {
-    $('.cities').show();
-    e.preventDefault();
-    var location = $('#autocomplete').val();
-    location = location.split(',')
-    console.log(location)
-    var city = location[0]
-    var state = location[1]
-    var country = location[2]
-    
-
-    // appSong.getMatchingCities(city);
-
-    appSong.getMatchingCities.forEach(function(compareCity) {
-      // if user selected city matches the values of the API
-      if (city === compareCity.city.displayName && compareCity.city.state.displayName) {
-        // run the getMetroID function
-        appSong.getMetroId(compareCity);
-        console.log(compareCity);
-      };
-    });
-
-
-
-  });
-
-} //appSong.usersLocation
-
-//STEP 3 --- might not need -- filtering auto complete
-// appSong.displayLocation = function(displayLocation) {
-// //display matching cities and have user select the correct one 
-// //displays the cities that match what the user inputting
-//   //$('.cities').empty();
-
-// //creates the radio buttons
-// //take the results of what they've inputted and use it to find the metroID
-// //creates a radio button for each possible result based on the name of the location
-
-//   displayLocation.forEach(function(locationChoice) {
-//     var $radioButtonsCity = $('<input>').attr({
-//       value: locationChoice.metroArea.id, 
-//       name: "locationChoice", 
-//       type: "radio",
-//       id: locationChoice.metroArea.id
-//     })
-//     var $radioLabel = $('<label>').text(locationChoice.city.displayName + ', ' + locationChoice.city.country.displayName).attr({
-//       for: locationChoice.metroArea.id
-//     })
-// // appends the options to the page in the form of radio buttons
-//     $('.cities').append($radioButtonsCity, $radioLabel);
-
-//   });
-
-//   console.log('drop down cities', displayLocation)
-// // takes the results from the location search and passes it along to the concert search
-//   appSong.findConcerts(displayLocation)
-// }
 
 
 //STEP 4
@@ -177,21 +119,21 @@ appSong.displayConcerts = function(concertsPlaying) {
 
     var $bandFilter = concertInfo.performance
 
-    $bandFilter.forEach(function(bandFilter) {
-      var $bandForm = $('<form>')
-      var $bandNames = $('<input>').attr({
-          value: bandFilter.displayName,
-          name: "bandNames",
-          type: "radio",
-          id: bandFilter.displayName
-        });
-      var $bandLabel = $('<label>').text(bandFilter.displayName).attr({
-          for: bandFilter.displayName
-        });
-      $bandForm.append($bandNames, $bandLabel)
-      $concertResults.append($bandForm)
+      $bandFilter.forEach(function(bandFilter) {
+        var $bandForm = $('<form>')
+        var $bandNames = $('<input>').attr({
+            value: bandFilter.displayName,
+            name: "bandNames",
+            type: "radio",
+            id: bandFilter.displayName
+          });
+        var $bandLabel = $('<label>').text(bandFilter.displayName).attr({
+            for: bandFilter.displayName
+          });
+        $bandForm.append($bandNames, $bandLabel)
+        $concertResults.append($bandForm)
 
-    })
+      })
 
     $('.bandSelection').append($concertResults)
 
@@ -208,7 +150,6 @@ appSong.matchBands = function(matchBands) {
     e.preventDefault();
     var bandPicked = $('input[type=radio]').val()
 
-    //spofity = $bandPicked
   });
 
   appSong.getSpotify(bandPicked)
@@ -255,3 +196,36 @@ appSong.init = function() {
 $(function() {
   appSong.init()
 });
+
+
+
+
+//STEP 3 --- might not need -- filtering auto complete
+// appSong.displayLocation = function(displayLocation) {
+// //display matching cities and have user select the correct one 
+// //displays the cities that match what the user inputting
+//   //$('.cities').empty();
+
+// //creates the radio buttons
+// //take the results of what they've inputted and use it to find the metroID
+// //creates a radio button for each possible result based on the name of the location
+
+//   displayLocation.forEach(function(locationChoice) {
+//     var $radioButtonsCity = $('<input>').attr({
+//       value: locationChoice.metroArea.id, 
+//       name: "locationChoice", 
+//       type: "radio",
+//       id: locationChoice.metroArea.id
+//     })
+//     var $radioLabel = $('<label>').text(locationChoice.city.displayName + ', ' + locationChoice.city.country.displayName).attr({
+//       for: locationChoice.metroArea.id
+//     })
+// // appends the options to the page in the form of radio buttons
+//     $('.cities').append($radioButtonsCity, $radioLabel);
+
+//   });
+
+//   console.log('drop down cities', displayLocation)
+// // takes the results from the location search and passes it along to the concert search
+//   appSong.findConcerts(displayLocation)
+// }
